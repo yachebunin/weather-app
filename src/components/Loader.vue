153e4fc2eade72b7d81167876ac934c5e131071f
Yuri Chebunin <chebuninyuri@gmail.com>
2022-07-16 17:39:28 +0300

    created loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loader">Загрузка...</div>
+</template>
+
+<script>
+export default {
+  name: "LoaderPage",
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/weather/Weather.vue
+++ src/components/weather/Weather.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="weather" v-if="weatherData">
     <Header :location="weatherData.location" @search="getWeather" />
-    <Body :current="weatherData.current" />
+    <Loader v-if="isLoading" />
+    <Body v-else :current="weatherData.current" />
   </div>
 </template>
 
 <script>
 import Header from "./Header.vue";
 import Body from "./Body.vue";
+import Loader from "../Loader.vue";
 
 export default {
   name: "WeatherMain",
   data() {
     return {
       weatherData: null,
+      isLoading: false,
     };
   },
   created() {
@@ -21,17 +24,21 @@ export default {
   },
   methods: {
     async getWeather(location = "Москва") {
+      this.isLoading = true;
+
       let response = await fetch(
         `http://api.weatherapi.com/v1/forecast.json?key=62cb304421ff4c07954121946221607&q=${location}&days=7&lang=ru`
       );
       let json = await response.json();
 
       this.weatherData = json;
+      this.isLoading = false;
     },
   },
   components: {
     Header,
-    Body
+    Body,
+    Loader,
   },
 };
 </script>
@@ -42,6 +49,8 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  width: 40%;
+  height: 40%;
   padding: 20px;
   background: rgba(0, 0, 0, 0.6);
   color: #fff;
